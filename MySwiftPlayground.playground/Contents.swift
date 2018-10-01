ad129609fc33f2b380ee2e8ca0d836e81ef1511d
RyanPerkins <bobefett.qwert12@gmail.com>
2018-10-01 08:55:21 -0600

    added a function with multiple outputs

--- MySwiftPlayground.playground/Contents.swift
+++ MySwiftPlayground.playground/Contents.swift
@@ -44,3 +44,23 @@ func map (number: Int) -> Int {
 
 print(map(number: 18))
 print(map(number: 27))
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int){
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores{
+        if score > max {
+            max = score
+        } else if score < min{
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print (statistics.sum)
